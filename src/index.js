b3fce8b34798074c3fadcc4632e8cbe87c39f22b
lnuvy <hangeulgod@naver.com>
2022-04-22 22:00:01 +0900

    필수패키지 설치 / 리액트18 권장 렌더링 적용

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/configureStore";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 리액트18 공식문서에서 권장하는 index 렌더링
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
+
+// CRA 초기 코드
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/redux/configureStore.js
@@ -0,0 +1,32 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { createBrowserHistory } from "history";
+import { connectRouter } from "connected-react-router";
+
+export const history = createBrowserHistory();
+
+// 리듀서 router 안에 history 넣기
+const rootReducer = combineReducers({
+  router: connectRouter(history),
+});
+
+const middlewares = [thunk.withExtraArgument({ history })];
+
+// 개발 환경일때, redux-logger 사용할 수 있게하기
+const env = process.env.NODE_ENV;
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
+
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+let store = (initialStore) => createStore(rootReducer, enhancer);
+
+export default store();
