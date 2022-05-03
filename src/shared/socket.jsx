7af67539412da7aada4a52f1b8274c30bbcffab0
lnuvy <hangeulgod@naver.com>
2022-05-03 18:08:07 +0900

    socket 작업 안하시면 shared->socket 주석처리 후 작업하세용

--- src/shared/socket.jsx
+++ src/shared/socket.jsx
@@ -2,3 +2,5 @@ import io from "socket.io-client";
 
 const URL = "http://localhost:5000";
 export const socket = io(URL, { transports: ["websocket"] });
+
+// 소켓 작업하지않으시는분들은 여기 싹다 주석처리하시고 작업하시면 소켓연결 에러 안뜹니다~
