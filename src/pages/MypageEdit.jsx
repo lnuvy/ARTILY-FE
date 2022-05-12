115001c3c29a8dde7fa19d5136506f99cb2ed793
lnuvy <hangeulgod@naver.com>
2022-05-13 00:32:41 +0900

    merge

--- src/pages/MypageEdit.jsx
+++ src/pages/MypageEdit.jsx
@@ -139,7 +139,7 @@ const MypageEdit = () => {
                 square
                 icon={
                   <Flex>
-                    <img src="../../public/images/Instagram.svg" alt="인스타" />
+                    <img src="/images/Instagram.svg" alt="인스타" />
                   </Flex>
                 }
                 fg="1"
@@ -164,7 +164,7 @@ const MypageEdit = () => {
               br="6px"
               icon={
                 <Flex>
-                  <img src="../../public/images/Behance.svg" alt="비핸스" />
+                  <img src="/images/Behance.svg" alt="비핸스" />
                 </Flex>
               }
               type="text"
@@ -186,7 +186,7 @@ const MypageEdit = () => {
                 br="6px"
                 icon={
                   <Flex>
-                    <img src="../../public/images/web.svg" alt="포트폴리오" />
+                    <img src="/images/web.svg" alt="포트폴리오" />
                   </Flex>
                 }
                 type="text"
