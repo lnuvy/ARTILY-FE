bd3a09210082b50ee3c57b6a6813a2cc64f20ba2
kimyeongkyung <klk17625@naver.com>
2022-05-30 01:51:32 +0900

    주석 일부 제거 commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,6 @@ const Home = () => {
   };
 
   const bannerImgs = [{ imageUrl: banner1 }, { imageUrl: banner2 }];
-  console.log("배포!!!!!!!!!!");
   return (
     <>
       <ImageCarouselBanner src={bannerImgs} />
